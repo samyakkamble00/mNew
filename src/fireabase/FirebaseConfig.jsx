96eb371383730be06dc3dd54a99acefc594c20d6
samyakkamble240500 <samyakk07@gmail.com>
2023-12-04 16:08:36 +0530

    first commit

--- src/fireabase/FirebaseConfig.jsx
+++ src/fireabase/FirebaseConfig.jsx
@@ -12,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "messthali-6d8fb.appspot.com",
   messagingSenderId: "1085344841512",
   appId: "1:1085344841512:web:8c4f4187c32252f3fe5a1d",
-  measurementId: "G-GKFSXZJW70"
+  measurementId: "G-GKFSXZJW70",
 };
 
 // Initialize Firebase
